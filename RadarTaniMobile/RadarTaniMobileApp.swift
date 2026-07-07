f7b73e7de36eddc043cd7bfcf0a87cb8fd446b83
HendraaaIrwn <hendraabuhilmi@gmail.com>
2026-07-08 05:32:28 +0700

    Initial Commit

---
+++ RadarTaniMobile/RadarTaniMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarTaniMobileApp.swift
+//  RadarTaniMobile
+//
+//  Created by Hendra Irawan on 08/07/26.
+//
+
+import SwiftUI
+
+@main
+struct RadarTaniMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
